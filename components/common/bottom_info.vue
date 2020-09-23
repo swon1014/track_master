46d09d9eeab397a4c883015fcf0831c99cc26e3b
swon1014 <62011338+swon1014@users.noreply.github.com>
2020-09-23 10:40:59 +0900

    Create bottom_info.vue

---
+++ components/common/bottom_info.vue
@@ -0,0 +1,8 @@
+<template>
+<div class="container-fluid bg-dark text-white" style="margin-top:50px;padding-top:30px;padding-bottom:30px">
+	<div class="container">
+		<p>Track Master / 문의 김성원(2068048) -
+		<a href="mailto: sungwon2.kim@sk.com">sungwon2.kim@sk.com</a></p>
+	</div>
+</div>
+</template>
